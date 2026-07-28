b6c5d3ca6465ee327f920f337c0d83311d2037ed
tattotatto <wuwenjun001@gmail.com>
2026-07-28 14:20:47 +0800

    fix: remove Prisma brand extension setPrototypeOf causing cyclic __proto__

--- apps/server/src/common/prisma/prisma.service.ts
+++ apps/server/src/common/prisma/prisma.service.ts
@@ -1,17 +1,12 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { ClsService } from 'nestjs-cls';
-import { brandIsolationExtension } from './prisma-brand.extension';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor(private cls: ClsService) {
-    super();
-  }
-
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
-    const extended = this.$extends(brandIsolationExtension(this.cls)) as unknown as this;
-    Object.setPrototypeOf(this, Object.getPrototypeOf(extended));
     await this.$connect();
   }
 
